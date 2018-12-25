da6b6db1cce1f4af9fb3a7216271ccce284ba2ce
tuke <tuke1993@gmail.com>
2018-12-25 16:17:49 +0800

    first commit

---
+++ src/main.py
@@ -0,0 +1,140 @@
+import os, sys
+import random
+import itertools
+
+import numpy as np
+import networkx as nx
+
+import utils
+
+def sample_graph(G, output_dir, times=10):
+    s_n = int(np.sqrt(G.number_of_nodes()))
+    for t in range(times):
+        t_dir = os.path.join(output_dir, 's{}'.format(t))
+        n = random.randint(int(s_n/2), 2*s_n)
+        Gs = utils.random_walk_induced_graph_sampling(G, n)
+        mapping = dict(zip(Gs.nodes(), range(Gs.number_of_nodes())))
+        Gs = nx.relabel_nodes(Gs, mapping)
+        file_path = os.path.join(t_dir, 'graph.edgelist')
+        label_path = os.path.join(t_dir, 'label.txt')
+        print("sample graph, nodes: {}, edges: {}, save into {}".format(Gs.number_of_nodes(), Gs.number_of_edges(), t_dir))
+        with utils.write_with_create(file_path) as f:
+            for i, j in Gs.edges():
+                print(i, j, file=f)
+        with utils.write_with_create(label_path) as f:
+            for i, data in Gs.nodes(data=True):
+                if 'label' in data:
+                    print(i, data['label'], file=f)
+
+
+def run_models(method, input_filename, output_dir, embedding_test_dir=None):
+    if method == 'node2vec':
+        emd_size = 128
+        params = {'p': [0.5, 1, 2],
+                  'q': [0.5, 1, 2],
+                  'num-walks': [10],
+                  'walk-length': [80],
+                  'window-size': [10]}
+        for v in itertools.product(*params.values()):
+            kargs = dict(zip(params.keys(), v))
+            utils.run_target_model(method, emd_size, input_filename, output_dir, embedding_test_dir=embedding_test_dir, **kargs)
+
+def main():
+    dataset_name = 'cora'
+    target_model = 'node2vec'
+    task = 'classification'
+    embedding_test_dir = '/home/tuke/embedding_test'
+    sampled_number = 10
+    steps = [5] # 0: sampling, 1: run target model, 2: test performance 3: run whole graph embedding 4: run meta learner
+
+    if 0 in steps:
+        dataset_path = 'data/{0}/{0}.edgelist'.format(dataset_name)
+        label_path = 'data/{0}/{0}_label.txt'.format(dataset_name)
+        G = utils.load_graph(dataset_path, label_path)
+        sample_graph(G, 'data/{0}/sampled'.format(dataset_name), times=sampled_number)
+    if 1 in steps:
+        for i in range(sampled_number):
+            input_filename = os.path.abspath('data/{}/sampled/s{}/graph.edgelist'.format(dataset_name, i))
+            output_dir = os.path.abspath('embeddings/{}/sampled/s{}'.format(dataset_name, i))
+            run_models(target_model, input_filename, output_dir, embedding_test_dir=embedding_test_dir)
+    if 2 in steps:
+        for i in range(sampled_number):
+            embedding_dir = os.path.abspath('embeddings/{}/sampled/s{}'.format(dataset_name, i))
+            save_dir = os.path.abspath('result/{}/sampled/s{}'.format(dataset_name, i))
+            if task == 'classification':
+                save_filename = os.path.join(save_dir, 'cf')
+            models = list(map(lambda a: os.path.join(embedding_dir, a),
+                                filter(lambda x: x.startswith(target_model), os.listdir(embedding_dir))))
+            labels = os.path.abspath('data/{}/sampled/s{}/label.txt'.format(dataset_name, i))
+            utils.run_test(task, dataset_name, models, labels, save_filename, embedding_test_dir)
+    if 3 in steps:
+        import netlsd
+        for i in range(sampled_number):
+            edgelist_filename = 'data/{}/sampled/s{}/graph.edgelist'.format(dataset_name, i)
+            label_name = 'data/{}/sampled/s{}/label.txt'.format(dataset_name, i)
+            save_path = 'embeddings/{}/sampled/s{}/wne.embeddings'.format(dataset_name, i)
+            G = utils.load_graph(edgelist_filename, label_name)
+            wne = netlsd.heat(G)
+            with utils.write_with_create(save_path) as f:
+                print(" ".join(map(str, wne)), file=f)
+        dataset_path = 'data/{0}/{0}.edgelist'.format(dataset_name)
+        label_path = 'data/{0}/{0}_label.txt'.format(dataset_name)
+        save_path = 'embeddings/{}/wne.embeddings'.format(dataset_name)
+        if not os.path.exists(save_path):
+            G = utils.load_graph(dataset_path, label_path)
+            wne = netlsd.heat(G)
+            with utils.write_with_create(save_path) as f:
+                print(" ".join(map(str, wne)), file=f)
+    if 4 in steps:
+        from sklearn import gaussian_process
+        X = []
+        y = []
+        for i in range(sampled_number):
+            data_dir = 'result/{}/sampled/s{}'.format(dataset_name, i)
+            cf = np.loadtxt(os.path.join(data_dir, 'cf'), dtype=float)
+            cf_name = open(os.path.join(data_dir, 'cf_names')).readlines()
+            params = np.array(list(map(lambda x: x.strip().split('_')[1:], cf_name))).astype(float)
+            if target_model == 'node2vec':
+                params = params[:, -2:]
+            wne = np.loadtxt('embeddings/{}/sampled/s{}/wne.embeddings'.format(dataset_name, i))
+            feats = np.hstack((params, np.tile(wne, (len(params), 1))))
+            X.append(feats)
+            y.append(cf)
+        X = np.vstack(X)
+        y = np.vstack(y)
+        gp = gaussian_process.GaussianProcessRegressor()
+        gp.fit(X, y[:, 1])
+        wne = np.loadtxt('embeddings/{}/wne.embeddings'.format(dataset_name, i))
+        X_test = []
+        print(y)
+        for p in np.linspace(0, 2, num=100):
+            for q in np.linspace(0, 2, num=100):
+                X_test.append(np.hstack(([p, q], wne)))
+        X_test = np.array(X_test)
+        #print(X_test)
+        y_test = gp.predict(X_test)
+        print("max y", np.max(y_test))
+        ind = np.argmax(y_test)
+        print(X_test[ind])
+    if 5 in steps:
+        input_filename = os.path.abspath('data/{0}/{0}.edgelist'.format(dataset_name))
+        output_dir = os.path.abspath('embeddings/{}'.format(dataset_name))
+        kargs = {'p': 0.00001,
+                  'q': 1.33333,
+                  'num-walks': 10,
+                  'walk-length': 80,
+                  'window-size': 10}
+        utils.run_target_model(target_model, 128, input_filename, output_dir, embedding_test_dir=embedding_test_dir, **kargs)
+
+        embedding_dir = output_dir
+        save_dir = os.path.abspath('result/{}'.format(dataset_name))
+        if task == 'classification':
+            save_filename = os.path.join(save_dir, 'cf')
+        models = list(map(lambda a: os.path.join(embedding_dir, a),
+                            filter(lambda x: x.startswith(target_model), os.listdir(embedding_dir))))
+        print(models)
+        labels = 'data/{0}/{0}_label.txt'.format(dataset_name)
+        utils.run_test(task, dataset_name, models, labels, save_filename, embedding_test_dir)
+
+if __name__ == '__main__':
+    main()
---
+++ src/utils.py
@@ -0,0 +1,76 @@
+import random
+import os, sys
+
+import networkx as nx
+import numpy as np
+from sklearn.cluster import SpectralClustering
+
+def split_network(G, N):
+    sc = SpectralClustering(N, affinity='precomputed')
+    return sc.fit_predict(nx.adjacency_matrix(G))
+
+def random_walk_induced_graph_sampling(G, N, T=100, growth_size=2):
+    # Refer to https://github.com/Ashish7129/Graph-Sampling
+    G = nx.convert_node_labels_to_integers(G, 0, 'default', True)
+    for n, data in G.nodes(data=True):
+        G.node[n]['id'] = n
+    n_node = G.number_of_nodes()
+    temp_node = random.randint(0, n_node-1)
+    sampled_nodes = set([G.node[temp_node]['id']])
+    iter_ = 1
+    nodes_before_t_iter = 0
+    curr_node = temp_node
+    while len(sampled_nodes) != N:
+        edges = [n for n in G.neighbors(curr_node)]
+        index_of_edge = random.randint(0, len(edges)-1)
+        chosen_node = edges[index_of_edge]
+        sampled_nodes.add(G.node[chosen_node]['id'])
+        curr_node = chosen_node
+        iter_ += 1
+        if iter_ % T == 0:
+            if (len(sampled_nodes)-nodes_before_t_iter < growth_size):
+                curr_node = random.randint(0, n_node-1)
+            nodes_before_t_iter = len(sampled_nodes)
+    sampled_graph = G.subgraph(sampled_nodes)
+    return sampled_graph
+
+def write_with_create(path):
+    dirpath = os.path.dirname(path)
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    return open(path, 'w')
+
+def load_graph(edgelist_filename, label_name=None):
+    G = nx.read_edgelist(edgelist_filename, nodetype=int)
+    if label_name is not None:
+        labels = np.loadtxt(label_name, dtype=int)
+        ### Warning:: The call order of arguments `values` and `name` switched between v1.x & v2.x.
+        nx.set_node_attributes(G, dict(labels), 'label')
+    print("load graph", G.number_of_nodes(), G.number_of_edges())
+    return G
+
+def run_target_model(method, emd_size, input_filename, output_dir, embedding_test_dir, **kargs):
+    sys.path.append(embedding_test_dir)
+    from src.baseline import baseline
+    with cd(embedding_test_dir):
+        baseline(method, None, emd_size, input_filename, output_dir, **kargs)
+
+def run_test(task, dataset_name, models, labels, save_filename, embedding_test_dir):
+    sys.path.append(embedding_test_dir)
+    from src.test import test
+    args = {'radio': [0.7]}
+    args['label_name'] = labels
+    with cd(embedding_test_dir):
+        test(task, None, dataset_name, models, save_filename=save_filename, **args)
+
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
